d0ec4a069341cc5cbc15088805814888a86d0dff
Florian Knip <florianknip@web.de>
2014-05-14 17:05:54 +0200

    set up project

---
+++ Canvas Stuff_files/stopWatch.js
@@ -0,0 +1,113 @@
+/* This is the stop Watch:
+		<!-- Created by Kåre Byberg © 21.01.2005. Please acknowledge if used on other domains than http://www.timpelen.com -->
+		<!-- Version 1.1 - 22.04.2006 -->
+		Link: http://proft.50megs.com/stopwatch2.html
+		
+		I modified it for the purposes of this gaming project
+**********************************************/
+		
+		var flagclock = 0;
+		var flagstop = 0;
+		var stoptime = 0;
+		var splitcounter = 0;
+		var currenttime;
+		var splitdate = '';
+		var output;
+		var clock;
+		
+		function startstop()
+		{
+			var startstop = document.getElementById('startstopbutton');
+			var startdate = new Date();
+			var starttime = startdate.getTime();
+			if(flagclock==0)
+			{
+				flagclock = 1;
+				counter(starttime);
+			}
+			else
+			{
+				flagclock = 0;
+				flagstop = 1;
+				splitdate = '';
+			}
+		}
+		function counter(starttime)
+		{
+			output = document.getElementById('output');
+			clock = document.getElementById('clock');
+			currenttime = new Date();
+			var timediff = currenttime.getTime() - starttime;
+			if(flagstop == 1)
+			{
+				timediff = timediff + stoptime
+			}
+			if(flagclock == 1)
+			{
+				clock.value = formattime(timediff,'');
+				refresh = setTimeout('counter(' + starttime + ');',10);
+			}
+			else
+			{
+				window.clearTimeout(refresh);
+				stoptime = timediff;
+			}
+		}
+		function formattime(rawtime,roundtype)
+		{
+			if(roundtype == 'round')
+			{
+				var ds = Math.round(rawtime/100) + '';
+			}
+			else
+			{
+				var ds = Math.floor(rawtime/100) + '';		
+			}
+			var sec = Math.floor(rawtime/1000);
+			var min = Math.floor(rawtime/60000);
+			ds = ds.charAt(ds.length - 1);
+			if(min >= 60)
+			{
+				startstop();
+			}
+			sec = sec - 60 * min + '';
+			if(sec.charAt(sec.length - 2) != '')
+			{
+				sec = sec.charAt(sec.length - 2) + sec.charAt(sec.length - 1);
+			}
+			else
+			{
+				sec = 0 + sec.charAt(sec.length - 1);
+			}	
+			min = min + '';
+			if(min.charAt(min.length - 2) != '')
+			{
+				min = min.charAt(min.length - 2)+min.charAt(min.length - 1);
+			}
+			else
+			{
+				min = 0 + min.charAt(min.length - 1);
+			}
+			return min + ':' + sec + ':' + ds;
+		}
+		
+		function resetclock()
+		{
+			flagstop = 0;
+			stoptime = 0;
+			splitdate = '';
+			window.clearTimeout(refresh);
+		
+			splitcounter = 0;
+			if(flagclock == 1)
+			{
+				var resetdate = new Date();
+				var resettime = resetdate.getTime();
+				counter(resettime);
+			}
+			else
+			{
+				clock.value = "00:00:0";
+			}
+		}
+/**********************************************************/
---
+++ Readme.txt
@@ -0,0 +1,15 @@
+This is another former Student Project.
+
+The project was assigned in the course 270 CIS Client-side Web Programming at Clarion University of Pennsylvania.
+
+Instructor was Dr. Joseph Wyatt 
+The Course Website can be found here http://jbwyatt.com/
+
+The main task was to use the HTML canvas to create a game.
+The idea was to modify an existing ski game and make it more fun by adding more features
+
+Within our work we added the following:
+- more obstacles
+- a turbo for skier
+- documentation of the .js code
+
